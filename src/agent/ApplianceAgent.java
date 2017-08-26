81fafcd136fdb82928dadf923b721508e91c446c
xxfast <isuru@xfastgames.com>
2017-08-26 18:47:46 +1000

    Now the appliance agents sends a notification message

--- src/agent/ApplianceAgent.java
+++ src/agent/ApplianceAgent.java
@@ -1,11 +1,13 @@
 package agent;
 
+import FIPA.DateTime;
 import behavior.DiscoverBehaviour;
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.OneShotBehaviour;
 import jade.lang.acl.ACLMessage;
+import model.NotificationMessage;
 
 public class ApplianceAgent extends Agent { 
 
@@ -15,14 +17,14 @@ public class ApplianceAgent extends Agent {
 		DiscoverBehaviour db = new DiscoverBehaviour();
 		db.setToDiscover(homeAgent.getName());
 		addBehaviour(db);
+		addBehaviour(new NotifyingBehavior());
 	}
 	
 	private class NotifyingBehavior extends OneShotBehaviour{
 		
 		@Override
 		public void action() {
-			ACLMessage notificationMessage = new ACLMessage( ACLMessage.INFORM );
-			notificationMessage.setContent("");
+			ACLMessage notificationMessage = new NotificationMessage(1,1,new DateTime());
 			notificationMessage.addReceiver( new AID(homeAgent.getName(), AID.ISLOCALNAME ) );
             send( notificationMessage );
 		}
