0465a91ee8f642b335acf0112278c39c941e2a44
xxfast <isuru@xfastgames.com>
2017-08-29 22:27:32 +1000

    Added the Retailer Agent
    updated the HomeAgent

---
+++ src/agent/RetailerAgent.java
@@ -0,0 +1,32 @@
+package agent;
+
+import agent.HomeAgent.NotificationRecievingBehavior;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.util.Logger;
+
+public class RetailerAgent extends TradeAgent {
+
+	private Logger myLogger = Logger.getMyLogger(getClass().getName());
+	
+	protected void setup() {
+		// Registration with the DF 
+		DFAgentDescription dfd = new DFAgentDescription();
+		ServiceDescription sd = new ServiceDescription();   
+		sd.setType("RetailerAgent"); 
+		sd.setName(getName());
+		sd.setOwnership("TradeNetwork");
+		dfd.setName(getAID());
+		dfd.addServices(sd);
+		try {
+			DFService.register(this,dfd);
+			NotificationRecievingBehavior notificationRecievingBehavior = new NotificationRecievingBehavior(this);
+			addBehaviour(notificationRecievingBehavior);
+		} catch (FIPAException e) {
+			myLogger.log(Logger.SEVERE, "Agent "+getLocalName()+" - Cannot register with DF", e);
+			doDelete();
+		}
+	}
+}
