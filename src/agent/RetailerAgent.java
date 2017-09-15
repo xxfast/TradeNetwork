d350cf5dbe00b968a56d63f5f1eece089b15cbeb
jery575 <jeremy.wall575@gmail.com>
2017-09-15 14:46:00 +1000

    Commented out error code
    Commented out some code imported from the HomeAgent that created an error.

--- src/agent/RetailerAgent.java
+++ src/agent/RetailerAgent.java
@@ -1,10 +1,12 @@
 package agent;
 
 import agent.HomeAgent.NotificationRecievingBehavior;
+import jade.core.AID;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
 import jade.util.Logger;
 
 public class RetailerAgent extends TradeAgent {
@@ -12,6 +14,7 @@ public class RetailerAgent extends TradeAgent {
 	private Logger myLogger = Logger.getMyLogger(getClass().getName());
 	
 	protected void setup() {
+		
 		// Registration with the DF 
 		DFAgentDescription dfd = new DFAgentDescription();
 		ServiceDescription sd = new ServiceDescription();   
@@ -20,13 +23,19 @@ public class RetailerAgent extends TradeAgent {
 		sd.setOwnership("TradeNetwork");
 		dfd.setName(getAID());
 		dfd.addServices(sd);
+		
 		try {
 			DFService.register(this,dfd);
-			NotificationRecievingBehavior notificationRecievingBehavior = new NotificationRecievingBehavior(this);
-			addBehaviour(notificationRecievingBehavior);
+			
+			// Does this need to be static in the home agent for this agent to use this?
+			//NotificationRecievingBehavior notificationRecievingBehavior = new NotificationRecievingBehavior(this);
+			//addBehaviour(notificationRecievingBehavior);
+			
 		} catch (FIPAException e) {
 			myLogger.log(Logger.SEVERE, "Agent "+getLocalName()+" - Cannot register with DF", e);
 			doDelete();
 		}
 	}
+	
+	
 }
