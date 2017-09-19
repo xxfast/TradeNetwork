0811bfef175a4921a75cf3e976bc5510c4e4433c
jery575 <jeremy.wall575@gmail.com>
2017-09-19 20:16:23 +1000

    Made Code a bit neater
    I've tried to make the code a bit neater before I add the contractNetResponder behaviour. Please let me know if this is okay

--- src/agent/RetailerAgent.java
+++ src/agent/RetailerAgent.java
@@ -1,12 +1,21 @@
 package agent;
 
-import agent.HomeAgent.NotificationRecievingBehavior;
-import jade.core.AID;
-import jade.domain.DFService;
+// Used to make the agent a Service Provider Agent
 import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.DFService;
+
+// Used to make the agent a ContractNetResponder Agent
 import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.proto.ContractNetResponder;
+import jade.domain.FIPANames;
+import jade.domain.FIPAAgentManagement.NotUnderstoodException;
+import jade.domain.FIPAAgentManagement.RefuseException;
+import jade.domain.FIPAAgentManagement.FailureException;
+
+// Used to log exceptions
 import jade.util.Logger;
 
 public class RetailerAgent extends TradeAgent {
@@ -14,28 +23,41 @@ public class RetailerAgent extends TradeAgent {
 	private Logger myLogger = Logger.getMyLogger(getClass().getName());
 	
 	protected void setup() {
+		// Describes the agent as a retail agent
+		setupServiceProviderComponent();
 		
-		// Registration with the DF 
+		// Add the ContractNetResponder behaviour
+		
+		
+		
+	}
+	
+	private void setupServiceProviderComponent () {
 		DFAgentDescription dfd = new DFAgentDescription();
-		ServiceDescription sd = new ServiceDescription();   
+		ServiceDescription sd = new ServiceDescription(); 
+		
+		// Maybe also add another service description of electricity buyer for when we implement agents buying power
+		
+		// Create service description
 		sd.setType("RetailerAgent"); 
 		sd.setName(getName());
 		sd.setOwnership("TradeNetwork");
+		
+		// Add service to agent description
 		dfd.setName(getAID());
 		dfd.addServices(sd);
 		
+		// Try to register the agent with its description
 		try {
-			DFService.register(this,dfd);
-			
-			// Does this need to be static in the home agent for this agent to use this?
-			//NotificationRecievingBehavior notificationRecievingBehavior = new NotificationRecievingBehavior(this);
-			//addBehaviour(notificationRecievingBehavior);
-			
+			DFService.register(this,dfd);	
 		} catch (FIPAException e) {
 			myLogger.log(Logger.SEVERE, "Agent "+getLocalName()+" - Cannot register with DF", e);
 			doDelete();
 		}
 	}
+	
+	
+	
 	
 	
 }
