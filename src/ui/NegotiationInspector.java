3e381264b996690b45d57dcc75497b603961b594
xxfast <isuru@xfastgames.com>
2017-11-06 17:50:41 +1100

    added the tabs

--- src/ui/NegotiationInspector.java
+++ src/ui/NegotiationInspector.java
@@ -10,12 +10,15 @@ import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JSlider;
+import javax.swing.JTabbedPane;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.ValueAxis;
 import org.jfree.ui.ApplicationFrame;
 import org.jfree.ui.RefineryUtilities;
 
@@ -26,21 +29,34 @@ import model.Offer;
 import negotiation.NegotiationThread;
 import negotiation.Strategy;
 
+import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 public class NegotiationInspector extends JFrame {
 	
 	private AgentDailyNegotiationThread toDisplay;
+	
+	private int selected = 0;
 
 	public NegotiationInspector(String applicationTitle, String chartTitle, AgentDailyNegotiationThread toDisplay) {
 		super(applicationTitle);
 		setToDisplay(toDisplay);
-		JFreeChart lineChart = ChartFactory.createLineChart(chartTitle, "Iteration", "Price", createDataset(0),
+		JFreeChart lineChart = ChartFactory.createLineChart(null, "Iteration", "Price", createDataset(0),
 				PlotOrientation.VERTICAL, true, true, false);
 
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
+		JTabbedPane tabs = new JTabbedPane();
+		for(AID agent : toDisplay.getAgents()) {
+	        JPanel jp = new JPanel();
+	        JLabel label = new JLabel();
+	        label.setText(chartTitle + " with " + agent.getLocalName());
+	        jp.add(label);
+	        tabs.addTab(agent.getLocalName(), jp);
+		}
+		this.getContentPane().add(tabs, BorderLayout.NORTH);
+		
 		ChartPanel chartPanel = new ChartPanel(lineChart);
 		chartPanel.setPreferredSize(new java.awt.Dimension(560, 367));
 		//setContentPane(chartPanel);
@@ -56,6 +72,13 @@ public class NegotiationInspector extends JFrame {
             		NegotiationInspector.this.validate();
             }
         });
+
+		tabs.addChangeListener(new ChangeListener() {
+	        public void stateChanged(ChangeEvent e) {
+	        		selected = tabs.getSelectedIndex();
+            		lineChart.getCategoryPlot().setDataset(createDataset(slider.getValue()));
+	        }
+	    });
         Box p = new Box(BoxLayout.X_AXIS);
         p.add(new JLabel("Time:"));
         p.add(slider);
@@ -66,13 +89,15 @@ public class NegotiationInspector extends JFrame {
 	private DefaultCategoryDataset createDataset(int hour) {
 		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
 		Map<AID, Map<Party,List<Offer>>> negotiations = getToDisplay().getNegotiationsForHour(hour);
-		for(AID agent : negotiations.keySet()) {
+		if(!negotiations.keySet().isEmpty()) {
+			AID agent = (AID) negotiations.keySet().toArray()[selected];
 			for(Party p : negotiations.get(agent).keySet()) {
 				for(Offer o : negotiations.get(agent).get(p)) {
-					dataset.addValue(o.getOfferValue(Strategy.Item.PRICE), (p==Party.SELF)?"Me"+agent.getLocalName():agent.getLocalName(), negotiations.get(agent).get(p).indexOf(o)+"");
+					dataset.addValue(o.getOfferValue(Strategy.Item.PRICE), (p==Party.SELF)?"Me":agent.getLocalName(), negotiations.get(agent).get(p).indexOf(o)+"");
 				}
 			}
 		}
+
 		return dataset;
 	}
 
